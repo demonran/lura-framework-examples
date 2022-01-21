57adc463bc674285bd3ef8cb63e3e1a8becacc84
ran.liu1 <ran.liu1@thoughtworks.com>
2022-01-21 20:42:04 +0800

    set enum type to string

--- ecommerce-product-service/src/main/kotlin/me/luraframework/example/product/model/Product.kt
+++ ecommerce-product-service/src/main/kotlin/me/luraframework/example/product/model/Product.kt
@@ -2,6 +2,8 @@ package me.luraframework.example.product.model
 
 import java.math.BigDecimal
 import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
 import javax.persistence.GeneratedValue
 import javax.persistence.GenerationType
 import javax.persistence.Id
@@ -18,6 +20,7 @@ data class Product(
   val cover: String,
   val stock: Long,
   val price: BigDecimal,
+  @Enumerated(EnumType.STRING)
   val status: ProductStatus,
   val shopId: Long
 )
